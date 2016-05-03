314ab6a38fbbf14cab537a657d3b1964654c68b1
Joseph Yi <joe@josephyi.com>
2016-05-03 16:51:16 -0700

    updating some libs, experimenting with mdl select component

--- web/static/js/components/App.js
+++ web/static/js/components/App.js
@@ -1,28 +1,32 @@
 import React from 'react'
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import {Textfield} from 'react-mdl'
+import 'mdl-ext'
 
 const App = (props) => (
-    <Layout>
-        <Header title="Title" scroll>
-            <Navigation>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Lis</a>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="">Link11111</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-        </Content>
-    </Layout>
+    <div>
+        <div className="mdl-selectfield mdl-js-selectfield mdl-selectfield--floating-label is-dirty">
+            <select id="region" className="mdl-selectfield__select" defaultValue="NA">
+                <option value="BR">BR</option>
+                <option value="EUNE">EUNE</option>
+                <option value="EUW">EUW</option>
+                <option value="KR">KR</option>
+                <option value="LAN">LAN</option>
+                <option value="LAS">LAS</option>
+                <option value="NA">NA</option>
+                <option value="OCE">OCE</option>
+                <option value="RU">RU</option>
+                <option value="TR">TR</option>
+                <option value="PBE">PBE</option>
+            </select>
+            <label className="mdl-selectfield__label" htmlFor="region">Region</label>
+        </div>
+        <Textfield
+            onChange={() => {}}
+            label="Summoner Name..."
+            floatingLabel
+            style={{width: '300px'}}
+        />
+    </div>
 );
 
 export default App;
