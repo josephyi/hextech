ec4da751953d100135059950dcb45406d6bd990f
Joseph Yi <joe@josephyi.com>
2016-05-06 17:00:03 -0700

    using bootstrap instead of mdl

--- web/static/js/components/App.js
+++ web/static/js/components/App.js
@@ -1,32 +1,53 @@
 import React from 'react'
-import {Textfield} from 'react-mdl'
-import 'mdl-ext'
-
+import {Button, ControlLabel, Form, FormGroup, FormControl, Navbar} from 'react-bootstrap'
 const App = (props) => (
-    <div>
-        <div className="mdl-selectfield mdl-js-selectfield mdl-selectfield--floating-label is-dirty">
-            <select id="region" className="mdl-selectfield__select" defaultValue="NA">
-                <option value="BR">BR</option>
-                <option value="EUNE">EUNE</option>
-                <option value="EUW">EUW</option>
-                <option value="KR">KR</option>
-                <option value="LAN">LAN</option>
-                <option value="LAS">LAS</option>
-                <option value="NA">NA</option>
-                <option value="OCE">OCE</option>
-                <option value="RU">RU</option>
-                <option value="TR">TR</option>
-                <option value="PBE">PBE</option>
-            </select>
-            <label className="mdl-selectfield__label" htmlFor="region">Region</label>
-        </div>
-        <Textfield
-            onChange={() => {}}
-            label="Summoner Name..."
-            floatingLabel
-            style={{width: '300px'}}
-        />
-    </div>
+<Navbar inverse>
+    <Navbar.Header>
+        <Navbar.Brand>
+            <a href="#">Hextech</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+        <Navbar.Form pullLeft>
+            <FormGroup controlId="formControlsSelect">
+                            <FormControl componentClass="select" defaultValue="NA">
+                                <option value="BR">BR</option>
+                                <option value="EUNE">EUNE</option>
+                                <option value="EUW">EUW</option>
+                                <option value="KR">KR</option>
+                                <option value="LAN">LAN</option>
+                                <option value="LAS">LAS</option>
+                                <option value="NA">NA</option>
+                                <option value="OCE">OCE</option>
+                                <option value="RU">RU</option>
+                                <option value="TR">TR</option>
+                                <option value="PBE">PBE</option>
+                            </FormControl>
+                        </FormGroup>
+            {' '}
+            <FormGroup controlId="summonerName">
+                <FormControl type="text" placeholder="Summoner Name" />
+            </FormGroup>
+            {' '}
+            <Button type="submit">Craft</Button>
+        </Navbar.Form>
+    </Navbar.Collapse>
+</Navbar>
+
+    // <div>
+    //     <Form inline>
+    //
+    //         {' '}
+    //         <FormGroup controlId="summonerName">
+    //             <FormControl type="text" placeholder="Summoner Name" />
+    //         </FormGroup>
+    //         {' '}
+    //         <Button>
+    //             Craft!
+    //         </Button>
+    //     </Form>
+    // </div>
 );
 
 export default App;
